6a7ab9c8b9706c1b1dd24df3ecf58df9d4cb7972
Florian Latifi <mail@florian-latifi.at>
2016-05-18 15:07:05 +0200

    Add back CompilationFinal annotation

--- projects/com.oracle.truffle.llvm.nodes.impl/src/com/oracle/truffle/llvm/nodes/impl/base/LLVMTerminatorNode.java
+++ projects/com.oracle.truffle.llvm.nodes.impl/src/com/oracle/truffle/llvm/nodes/impl/base/LLVMTerminatorNode.java
@@ -29,6 +29,7 @@
  */
 package com.oracle.truffle.llvm.nodes.impl.base;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.instrumentation.Instrumentable;
 import com.oracle.truffle.api.source.SourceSection;
@@ -47,6 +48,7 @@ import com.oracle.truffle.llvm.nodes.base.LLVMStatementNode;
 @Instrumentable(factory = LLVMTerminatorNodeWrapper.class)
 public abstract class LLVMTerminatorNode extends LLVMStatementNode {
 
+    @CompilerDirectives.CompilationFinal
     private final int[] successors;
 
     /**
