b41d725bef574ac8cd41cf41f3d0e132a666fc03
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2016-10-04 14:04:07 +0200

    Fix some code formating errors

--- projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/adapters/LLVMToBitcodeAdapter.java
+++ projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/adapters/LLVMToBitcodeAdapter.java
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2016, Oracle and/or its affiliates.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.oracle.truffle.llvm.parser.adapters;
 
 import java.math.BigInteger;
@@ -94,21 +123,26 @@ public class LLVMToBitcodeAdapter {
         return new FunctionType(returnType, args.toArray(new Type[args.size()]), hasVararg);
     }
 
+    private static final int HALF_SIZE = 16;
+    private static final int X86_FP80_SIZE = 80;
+    private static final int FP128_SIZE = 128;
+
     public static Type resolveType(ResolvedFloatingType type) {
         switch (type.getBits().intValue()) {
-            case 16:
+
+            case HALF_SIZE:
                 return FloatingPointType.HALF;
 
-            case 32:
+            case Float.SIZE:
                 return FloatingPointType.FLOAT;
 
-            case 64:
+            case Double.SIZE:
                 return FloatingPointType.DOUBLE;
 
-            case 80:
+            case X86_FP80_SIZE:
                 return FloatingPointType.X86_FP80;
 
-            case 128:
+            case FP128_SIZE:
                 return FloatingPointType.FP128;
 
             default:
@@ -118,20 +152,19 @@ public class LLVMToBitcodeAdapter {
 
     public static Type resolveType(ResolvedIntegerType type) {
         switch (type.getBits().intValue()) {
-
             case 1:
                 return IntegerType.BOOLEAN;
 
-            case 8:
+            case Byte.SIZE:
                 return IntegerType.BYTE;
 
-            case 16:
+            case Short.SIZE:
                 return IntegerType.SHORT;
 
-            case 32:
+            case Integer.SIZE:
                 return IntegerType.INTEGER;
 
-            case 64:
+            case Long.SIZE:
                 return IntegerType.LONG;
 
             default:
