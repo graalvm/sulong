0e418c17b60080cebe5f09c21124537e921b82d9
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2016-12-20 11:21:17 +0100

    Fix 'mx su-checks' test error

--- projects/com.oracle.truffle.llvm.writer/src/com/oracle/truffle/llvm/writer/tests/BinaryVectorOperatorTest.java
+++ projects/com.oracle.truffle.llvm.writer/src/com/oracle/truffle/llvm/writer/tests/BinaryVectorOperatorTest.java
@@ -1,6 +1,35 @@
+/*
+ * Copyright (c) 2016, Oracle and/or its affiliates.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.oracle.truffle.llvm.writer.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -10,7 +39,8 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.*;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.oracle.truffle.llvm.parser.api.model.enums.BinaryOperator;
 import com.oracle.truffle.llvm.parser.api.model.enums.CompareOperator;
@@ -33,7 +63,7 @@ public class BinaryVectorOperatorTest {
         this.operator = operator;
     }
 
-    @Parameters(name = "{index}: BinaryVectorOperator[type={0} ,operator={1}]")
+    @Parameters(name = "{index}: BinaryVectorOperator[type={0}, operator={1}]")
     public static Collection<Object[]> data() {
         List<Object[]> parameters = new LinkedList<>();
 
@@ -55,77 +85,77 @@ public class BinaryVectorOperatorTest {
     private static final long VECTOR2_1 = 200560490131L; // prim
     private static final long VECTOR2_2 = 1442968193L; // prim
 
+    // Checkstyle: stop magic number name check
     @Test
     public void test() {
-        long max_value = type.getBits() < 64 ? 1L << type.getBits() : Long.MAX_VALUE;
+        long maxValue = type.getBits() < 64 ? 1L << type.getBits() : Long.MAX_VALUE;
 
-        long vector1_1 = VECTOR1_1 % max_value;
-        long vector1_2 = VECTOR1_2 % max_value;
-        long vector2_1 = VECTOR2_1 % max_value;
-        long vector2_2 = VECTOR2_2 % max_value;
+        long vector11 = VECTOR1_1 % maxValue;
+        long vector12 = VECTOR1_2 % maxValue;
+        long vector21 = VECTOR2_1 % maxValue;
+        long vector22 = VECTOR2_2 % maxValue;
 
         long result1 = 0, result2 = 0;
 
         // TODO: signed/unsigned?
         switch (operator) {
             case INT_ADD:
-                result1 = (vector1_1 + vector2_1) % max_value;
-                result2 = (vector1_2 + vector2_2) % max_value;
+                result1 = (vector11 + vector21) % maxValue;
+                result2 = (vector12 + vector22) % maxValue;
                 break;
             case INT_SUBTRACT:
-                result1 = (vector1_1 - vector2_1) % max_value;
-                result2 = (vector1_2 - vector2_2) % max_value;
+                result1 = (vector11 - vector21) % maxValue;
+                result2 = (vector12 - vector22) % maxValue;
                 break;
             case INT_MULTIPLY:
-                result1 = (vector1_1 * vector2_1) % max_value;
-                result2 = (vector1_2 * vector2_2) % max_value;
+                result1 = (vector11 * vector21) % maxValue;
+                result2 = (vector12 * vector22) % maxValue;
                 break;
             case INT_UNSIGNED_DIVIDE:
-                result1 = (vector1_1 / vector2_1) % max_value;
-                result2 = (vector1_2 / vector2_2) % max_value;
+                result1 = (vector11 / vector21) % maxValue;
+                result2 = (vector12 / vector22) % maxValue;
                 break;
             case INT_SIGNED_DIVIDE:
-                result1 = (vector1_1 / vector2_1) % max_value;
-                result2 = (vector1_2 / vector2_2) % max_value;
+                result1 = (vector11 / vector21) % maxValue;
+                result2 = (vector12 / vector22) % maxValue;
                 break;
             case INT_UNSIGNED_REMAINDER:
-                result1 = (vector1_1 % vector2_1) % max_value;
-                result2 = (vector1_2 % vector2_2) % max_value;
+                result1 = (vector11 % vector21) % maxValue;
+                result2 = (vector12 % vector22) % maxValue;
                 break;
             case INT_SIGNED_REMAINDER:
-                result1 = (vector1_1 % vector2_1) % max_value;
-                result2 = (vector1_2 % vector2_2) % max_value;
+                result1 = (vector11 % vector21) % maxValue;
+                result2 = (vector12 % vector22) % maxValue;
                 break;
             case INT_SHIFT_LEFT:
-                result1 = (vector1_1 << vector2_1) % max_value;
-                result2 = (vector1_2 << vector2_2) % max_value;
+                result1 = (vector11 << vector21) % maxValue;
+                result2 = (vector12 << vector22) % maxValue;
                 break;
             case INT_LOGICAL_SHIFT_RIGHT:
-                result1 = (vector1_1 >> vector2_1) % max_value;
-                result2 = (vector1_2 >> vector2_2) % max_value;
+                result1 = (vector11 >> vector21) % maxValue;
+                result2 = (vector12 >> vector22) % maxValue;
                 break;
             case INT_ARITHMETIC_SHIFT_RIGHT:
-                result1 = (vector1_1 >>> vector2_1) % max_value;
-                result2 = (vector1_2 >>> vector2_2) % max_value;
+                result1 = (vector11 >>> vector21) % maxValue;
+                result2 = (vector12 >>> vector22) % maxValue;
                 break;
             case INT_AND:
-                result1 = (vector1_1 & vector2_1) % max_value;
-                result2 = (vector1_2 & vector2_2) % max_value;
+                result1 = (vector11 & vector21) % maxValue;
+                result2 = (vector12 & vector22) % maxValue;
                 break;
             case INT_OR:
-                result1 = (vector1_1 | vector2_1) % max_value;
-                result2 = (vector1_2 | vector2_2) % max_value;
+                result1 = (vector11 | vector21) % maxValue;
+                result2 = (vector12 | vector22) % maxValue;
                 break;
             case INT_XOR:
-                result1 = (vector1_1 ^ vector2_1) % max_value;
-                result2 = (vector1_2 ^ vector2_2) % max_value;
+                result1 = (vector11 ^ vector21) % maxValue;
+                result2 = (vector12 ^ vector22) % maxValue;
                 break;
             default:
                 fail("unexpected operator");
                 break;
         }
 
-        // Checkstyle: stop magic number name check
         InstructionGeneratorFacade facade = new InstructionGeneratorFacade("main", 1, IntegerType.BOOLEAN, false);
 
         Instruction vec1 = facade.createAllocate(new VectorType(type, 2)); // TODO: wrong align
@@ -133,12 +163,12 @@ public class BinaryVectorOperatorTest {
         Instruction vec_res = facade.createAllocate(new VectorType(type, 2));
 
         vec1 = facade.createLoad(vec1);
-        vec1 = facade.createInsertElement(vec1, new IntegerConstant(type, vector1_1), 0);
-        vec1 = facade.createInsertElement(vec1, new IntegerConstant(type, vector1_2), 1);
+        vec1 = facade.createInsertElement(vec1, new IntegerConstant(type, vector11), 0);
+        vec1 = facade.createInsertElement(vec1, new IntegerConstant(type, vector12), 1);
 
         vec2 = facade.createLoad(vec2);
-        vec2 = facade.createInsertElement(vec2, new IntegerConstant(type, vector2_1), 0);
-        vec2 = facade.createInsertElement(vec2, new IntegerConstant(type, vector2_2), 1);
+        vec2 = facade.createInsertElement(vec2, new IntegerConstant(type, vector21), 0);
+        vec2 = facade.createInsertElement(vec2, new IntegerConstant(type, vector22), 1);
 
         Instruction retVec = facade.createBinaryOperation(vec1, vec2, operator);
 
