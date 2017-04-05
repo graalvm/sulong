dd49407af8648fab2c226661942bb48811586ee8
Jacob Kreindl <jacob.kreindl@jku.at>
2017-04-05 15:04:54 +0200

    Parse the CMPXCHG Instruction

--- projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/LLVMBitcodeInstructionVisitor.java
+++ projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/LLVMBitcodeInstructionVisitor.java
@@ -51,6 +51,7 @@ import com.oracle.truffle.llvm.parser.model.symbols.instructions.BinaryOperation
 import com.oracle.truffle.llvm.parser.model.symbols.instructions.BranchInstruction;
 import com.oracle.truffle.llvm.parser.model.symbols.instructions.CallInstruction;
 import com.oracle.truffle.llvm.parser.model.symbols.instructions.CastInstruction;
+import com.oracle.truffle.llvm.parser.model.symbols.instructions.CompareExchangeInstruction;
 import com.oracle.truffle.llvm.parser.model.symbols.instructions.CompareInstruction;
 import com.oracle.truffle.llvm.parser.model.symbols.instructions.ConditionalBranchInstruction;
 import com.oracle.truffle.llvm.parser.model.symbols.instructions.ExtractElementInstruction;
@@ -244,6 +245,11 @@ final class LLVMBitcodeInstructionVisitor implements InstructionVisitor {
     public void visit(ResumeInstruction resumeInstruction) {
         LLVMControlFlowNode resume = factoryFacade.createResumeInstruction(runtime, method.getExceptionSlot());
         method.addTerminatingInstruction(resume, block.getBlockIndex(), block.getName());
+    }
+
+    @Override
+    public void visit(CompareExchangeInstruction cmpxchg) {
+        throw new UnsupportedOperationException("Not supported yet!");
     }
 
     @Override
