a0faf8c81e6e6950a2fbdf57ebeb8444b94e62a0
Daniel Pekarek <pekarekdaniel@gmail.com>
2017-08-16 15:42:08 +0200

    Simplify long multiplication

--- projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/asm/support/LongMultiplication.java
+++ projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/asm/support/LongMultiplication.java
@@ -37,40 +37,18 @@ public class LongMultiplication {
     private static final long MASK32 = 0xFFFFFFFFL;
     private static final int SHIFT32 = 32;
 
-    /**
-     * Returns as a {@code long} the most significant 64 bits of the 128-bit product of two 64-bit
-     * factors.
-     *
-     * @param x the first value
-     * @param y the second value
-     * @return the result
-     */
-    public static long multiplyHigh(long x, long y) {
-        if (x < 0 || y < 0) {
-            // Use technique from section 8-2 of Henry S. Warren, Jr.,
-            // Hacker's Delight (2nd ed.) (Addison Wesley, 2013), 173-174.
-            long x1 = x >> SHIFT32;
-            long x2 = x & MASK32;
-            long y1 = y >> SHIFT32;
-            long y2 = y & MASK32;
-            long z2 = x2 * y2;
-            long t = x1 * y2 + (z2 >>> SHIFT32);
-            long z1 = t & MASK32;
-            long z0 = t >> SHIFT32;
-            z1 += x2 * y1;
-            return x1 * y1 + z0 + (z1 >> SHIFT32);
-        } else {
-            // Use Karatsuba technique with two base 2^32 digits.
-            long x1 = x >>> SHIFT32;
-            long y1 = y >>> SHIFT32;
-            long x2 = x & MASK32;
-            long y2 = y & MASK32;
-            long a = x1 * y1;
-            long b = x2 * y2;
-            long c = (x1 + x2) * (y1 + y2);
-            long k = c - a - b;
-            return (((b >>> SHIFT32) + k) >>> SHIFT32) + a;
-        }
+    // based on http://www.hackersdelight.org/hdcodetxt/mulhu.c.txt
+    public static long multiplyHigh(long u, long v) {
+        long u0 = u & MASK32;
+        long u1 = u >> SHIFT32;
+        long v0 = v & MASK32;
+        long v1 = v >> SHIFT32;
+        long w0 = u0 * v0;
+        long t = u1 * v0 + (w0 >>> SHIFT32);
+        long w1 = t & MASK32;
+        long w2 = t >> SHIFT32;
+        w1 += u0 * v1;
+        return u1 * v1 + w2 + (w1 >> SHIFT32);
     }
 
     public static long multiplyHighUnsigned(long x, long y) {
