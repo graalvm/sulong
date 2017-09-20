c5ece5dc0ea375b18c3b277fc41d047c525874ea
Daniel Pekarek <pekarekdaniel@gmail.com>
2017-09-20 13:36:27 +0200

    Implement syscall nodes

--- projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/asm/syscall/LLVMAMD64SyscallNode.java
+++ projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/asm/syscall/LLVMAMD64SyscallNode.java
@@ -29,166 +29,97 @@
  */
 package com.oracle.truffle.llvm.nodes.asm.syscall;
 
-import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeChildren;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.profiles.LongValueProfile;
-import com.oracle.truffle.llvm.nodes.asm.support.LLVMAMD64String;
-import com.oracle.truffle.llvm.runtime.LLVMAddress;
 import com.oracle.truffle.llvm.runtime.LLVMExitException;
 import com.oracle.truffle.llvm.runtime.nodes.api.LLVMExpressionNode;
 
 @NodeChildren({@NodeChild("rax"), @NodeChild("rdi"), @NodeChild("rsi"), @NodeChild("rdx"), @NodeChild("r10"), @NodeChild("r8"), @NodeChild("r9")})
 public abstract class LLVMAMD64SyscallNode extends LLVMExpressionNode {
-    private final LongValueProfile profile = LongValueProfile.createIdentityProfile();
+    protected static final int NUM_SYSCALLS = 332;
 
     protected static void exit(int code) {
         throw new LLVMExitException(code);
     }
 
-    @Specialization
-    protected long executeI64(long rax, LLVMAddress rdi, long rsi, long rdx, long r10, long r8, long r9) {
-        switch ((int) profile.profile(rax)) {
-            case LLVMAMD64Syscall.SYS_open:
-                return LLVMAMD64File.open(LLVMAMD64String.cstr(rdi), (int) rsi, (int) rdx);
-            case LLVMAMD64Syscall.SYS_mmap:
-                return LLVMAMD64Memory.mmap(rdi, rsi, (int) rdx, (int) r10, (int) r8, r9);
-            case LLVMAMD64Syscall.SYS_brk:
-                return LLVMAMD64Memory.brk(rdi);
-            case LLVMAMD64Syscall.SYS_uname:
-                return LLVMAMD64Info.uname(rdi);
-            case LLVMAMD64Syscall.SYS_getcwd: {
-                String cwd = LLVMAMD64Path.getcwd();
-                if (cwd.length() >= rsi) {
-                    return -LLVMAMD64Error.ERANGE;
-                } else {
-                    LLVMAMD64String.strcpy(cwd, rdi);
-                    return cwd.length() + 1;
-                }
-            }
-            default:
-                // return -LLVMAMD64Error.ENOSYS;
-                CompilerDirectives.transferToInterpreter();
-                throw new RuntimeException("unknown syscall " + rax);
-        }
-    }
-
-    @SuppressWarnings("unused")
-    @Specialization
-    protected long executeI64(long rax, long rdi, LLVMAddress rsi, long rdx, long r10, long r8, long r9) {
-        switch ((int) profile.profile(rax)) {
-            case LLVMAMD64Syscall.SYS_read: {
-                // byte[] buf = new byte[(int) rdx];
-                // int result = LLVMAMD64File.read((int) rdi, buf);
-                // if (result > 0) {
-                // LLVMAMD64String.memcpy(buf, rsi, result);
-                // }
-                // return result;
-                return LLVMAMD64File.read((int) rdi, rsi, (int) rdx);
-            }
-            case LLVMAMD64Syscall.SYS_write:
-                // return LLVMAMD64File.write((int) rdi, LLVMAMD64String.memcpy(rsi, (int) rdx));
-                return LLVMAMD64File.write((int) rdi, rsi, (int) rdx);
-            case LLVMAMD64Syscall.SYS_fstat:
-                return -LLVMAMD64Error.ENOSYS;
-            case LLVMAMD64Syscall.SYS_readv:
-                return LLVMAMD64File.readv((int) rdi, rsi, rdx);
-            case LLVMAMD64Syscall.SYS_writev:
-                return LLVMAMD64File.writev((int) rdi, rsi, rdx);
-            case LLVMAMD64Syscall.SYS_clock_gettime:
-                return LLVMAMd64Time.clockGetTime((int) rdi, rsi);
-            default:
-                // return -LLVMAMD64Error.ENOSYS;
-                CompilerDirectives.transferToInterpreter();
-                throw new RuntimeException("unknown syscall " + rax);
-        }
-    }
-
-    @SuppressWarnings("unused")
-    @Specialization
-    protected long executeI64(long rax, LLVMAddress rdi, LLVMAddress rsi, long rdx, long r10, long r8, long r9) {
-        switch ((int) profile.profile(rax)) {
-            case LLVMAMD64Syscall.SYS_stat:
-                return LLVMAMD64File.stat(rdi, rsi);
-            case LLVMAMD64Syscall.SYS_lstat:
-                return -LLVMAMD64Error.ENOSYS;
-            default:
-                CompilerDirectives.transferToInterpreter();
-                throw new RuntimeException("unknown syscall " + rax);
-        }
-    }
-
-    @SuppressWarnings("unused")
-    @Specialization
-    protected long executeI64(long rax, long rdi, LLVMAddress rsi, LLVMAddress rdx, long r10, long r8, long r9) {
-        switch ((int) profile.profile(rax)) {
-            case LLVMAMD64Syscall.SYS_rt_sigaction:
-            case LLVMAMD64Syscall.SYS_rt_sigprocmask:
-                return -LLVMAMD64Error.ENOSYS;
-            default:
-                CompilerDirectives.transferToInterpreter();
-                throw new RuntimeException("unknown syscall " + rax);
-        }
-    }
-
-    @Specialization
-    protected long executeI64(long rax, long rdi, long rsi, long rdx, long r10, long r8, long r9) {
-        switch ((int) profile.profile(rax)) {
-            case LLVMAMD64Syscall.SYS_close:
-                return LLVMAMD64File.close((int) rdi);
-            case LLVMAMD64Syscall.SYS_lseek:
-                return LLVMAMD64File.lseek((int) rdi, rsi, (int) rdx);
-            case LLVMAMD64Syscall.SYS_ioctl:
-                return LLVMAMD64File.ioctl((int) rdi, (int) rsi, rdx);
-            case LLVMAMD64Syscall.SYS_dup2:
-                return LLVMAMD64File.dup2((int) rdi, (int) rsi);
-            case LLVMAMD64Syscall.SYS_getpid:
-                return LLVMAMD64Info.getpid();
-            case LLVMAMD64Syscall.SYS_exit_group:
-                // TODO: implement difference to SYS_exit
-            case LLVMAMD64Syscall.SYS_exit:
-                exit((int) rdi);
-                return 0;
-            case LLVMAMD64Syscall.SYS_fcntl:
-                return LLVMAMD64File.fcntl((int) rdi, (int) rsi, rdx);
-            case LLVMAMD64Syscall.SYS_ftruncate:
-                return LLVMAMD64File.ftruncate((int) rdi, rsi);
-            case LLVMAMD64Syscall.SYS_getuid:
-                return LLVMAMD64Security.getuid();
-            case LLVMAMD64Syscall.SYS_getgid:
-                return LLVMAMD64Security.getgid();
-            case LLVMAMD64Syscall.SYS_setuid:
-            case LLVMAMD64Syscall.SYS_setgid:
-                return -LLVMAMD64Error.ENOSYS;
-            case LLVMAMD64Syscall.SYS_getppid:
-                return LLVMAMD64Info.getppid();
-            case LLVMAMD64Syscall.SYS_futex:
-                return -LLVMAMD64Error.ENOSYS;
-            // Type compatibility wrappers
-            case LLVMAMD64Syscall.SYS_open:
-            case LLVMAMD64Syscall.SYS_mmap:
-            case LLVMAMD64Syscall.SYS_brk:
-            case LLVMAMD64Syscall.SYS_uname:
-            case LLVMAMD64Syscall.SYS_getcwd:
-                return executeI64(rax, LLVMAddress.fromLong(rdi), rsi, rdx, r10, r8, r9);
+    protected static LLVMAMD64SyscallOperationNode createNode(long rax) {
+        switch ((int) rax) {
             case LLVMAMD64Syscall.SYS_read:
+                return LLVMAMD64SyscallReadNodeGen.create();
             case LLVMAMD64Syscall.SYS_write:
-            case LLVMAMD64Syscall.SYS_fstat:
-            case LLVMAMD64Syscall.SYS_readv:
-            case LLVMAMD64Syscall.SYS_writev:
-            case LLVMAMD64Syscall.SYS_clock_gettime:
-                return executeI64(rax, rdi, LLVMAddress.fromLong(rsi), rdx, r10, r8, r9);
+                return LLVMAMD64SyscallWriteNodeGen.create();
+            case LLVMAMD64Syscall.SYS_open:
+                return LLVMAMD64SyscallOpenNodeGen.create();
+            case LLVMAMD64Syscall.SYS_close:
+                return new LLVMAMD64SyscallCloseNode();
             case LLVMAMD64Syscall.SYS_stat:
+                return LLVMAMD64SyscallStatNodeGen.create();
+            case LLVMAMD64Syscall.SYS_fstat:
+                return LLVMAMD64SyscallFstatNodeGen.create();
             case LLVMAMD64Syscall.SYS_lstat:
-                return executeI64(rax, LLVMAddress.fromLong(rdi), LLVMAddress.fromLong(rsi), rdx, r10, r8, r9);
+                return LLVMAMD64SyscallLstatNodeGen.create();
+            case LLVMAMD64Syscall.SYS_lseek:
+                return new LLVMAMD64SyscallLseekNode();
+            case LLVMAMD64Syscall.SYS_mmap:
+                return LLVMAMD64SyscallMmapNodeGen.create();
+            case LLVMAMD64Syscall.SYS_brk:
+                return LLVMAMD64SyscallBrkNodeGen.create();
             case LLVMAMD64Syscall.SYS_rt_sigaction:
+                return LLVMAMD64SyscallRtSigactionNodeGen.create();
             case LLVMAMD64Syscall.SYS_rt_sigprocmask:
-                return executeI64(rax, rdi, LLVMAddress.fromLong(rsi), LLVMAddress.fromLong(rdx), r10, r8, r9);
+                return LLVMAMD64SyscallRtSigprocmaskNodeGen.create();
+            case LLVMAMD64Syscall.SYS_ioctl:
+                return LLVMAMD64SyscallIoctlNodeGen.create();
+            case LLVMAMD64Syscall.SYS_readv:
+                return LLVMAMD64SyscallReadvNodeGen.create();
+            case LLVMAMD64Syscall.SYS_writev:
+                return LLVMAMD64SyscallWritevNodeGen.create();
+            case LLVMAMD64Syscall.SYS_dup2:
+                return new LLVMAMD64SyscallDup2Node();
+            case LLVMAMD64Syscall.SYS_getpid:
+                return new LLVMAMD64SyscallGetpidNode();
+            case LLVMAMD64Syscall.SYS_exit:
+            case LLVMAMD64Syscall.SYS_exit_group: // TODO: implement difference to SYS_exit
+                return new LLVMAMD64SyscallExitNode();
+            case LLVMAMD64Syscall.SYS_uname:
+                return LLVMAMD64SyscallUnameNodeGen.create();
+            case LLVMAMD64Syscall.SYS_fcntl:
+                return LLVMAMD64SyscallFcntlNodeGen.create();
+            case LLVMAMD64Syscall.SYS_ftruncate:
+                return new LLVMAMD64SyscallFtruncateNode();
+            case LLVMAMD64Syscall.SYS_getcwd:
+                return LLVMAMD64SyscallGetcwdNodeGen.create();
+            case LLVMAMD64Syscall.SYS_getuid:
+                return new LLVMAMD64SyscallGetuidNode();
+            case LLVMAMD64Syscall.SYS_getgid:
+                return new LLVMAMD64SyscallGetgidNode();
+            case LLVMAMD64Syscall.SYS_setuid:
+                return new LLVMAMD64SyscallSetuidNode();
+            case LLVMAMD64Syscall.SYS_setgid:
+                return new LLVMAMD64SyscallSetgidNode();
+            case LLVMAMD64Syscall.SYS_getppid:
+                return new LLVMAMD64SyscallGetPpidNode();
+            case LLVMAMD64Syscall.SYS_futex:
+                return LLVMAMD64SyscallFutexNodeGen.create();
+            case LLVMAMD64Syscall.SYS_clock_gettime:
+                return LLVMAMD64SyscallClockGetTimeNodeGen.create();
             default:
-                // return -LLVMAMD64Error.ENOSYS;
-                CompilerDirectives.transferToInterpreter();
-                throw new RuntimeException("unknown syscall " + rax);
+                return new LLVMAMD64UnknownSyscallNode((int) rax);
         }
+    }
+
+    @Specialization(guards = "rax == cachedRax", limit = "NUM_SYSCALLS")
+    protected long cachedSyscall(@SuppressWarnings("unused") long rax, Object rdi, Object rsi, Object rdx, Object r10, Object r8, Object r9,
+                    @Cached("createNode(rax)") LLVMAMD64SyscallOperationNode node, @SuppressWarnings("unused") @Cached("rax") long cachedRax) {
+        return node.execute(rdi, rsi, rdx, r10, r8, r9);
+    }
+
+    @SuppressWarnings("unused")
+    @Specialization(replaces = "cachedSyscall")
+    protected long executeI64(long rax, Object rdi, Object rsi, Object rdx, Object r10, Object r8, Object r9) {
+        // TODO: implement big switch with type casts + logic + ...?
+        throw new AssertionError("this should not happen (inline cache not big enough)");
     }
 }
